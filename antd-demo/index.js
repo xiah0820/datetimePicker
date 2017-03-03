8ff56e36ba419986c66fc1953bd546da35a16876
WangYundi <wangyundi0820@163.com>
2017-03-03 16:36:52 +0800

    add the whole project.

---
+++ antd-demo/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DatePicker, message } from 'antd';
+import moment from 'moment';
+
+// It's recommended to set locale in entry file globaly.
+import 'moment/locale/zh-cn';
+moment.locale('zh-cn');
+
+var endDate = Math.round(new Date(2017,7,1,0,0,0));
+
+function disabledDate(current) {
+  // can not select days before today and today
+  if (!current){
+  	return false;
+  }
+  else{
+  	 return current.valueOf() > endDate || current.valueOf() < Date.now();
+  }
+ 
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '',
+    };
+  }
+  handleChange(date) {
+    message.info('Selected Date: ' + date.toString());
+    this.setState({ date });
+  }
+  render() {
+    return (
+      <div style={{ width: 400, margin: '100px auto' }}>
+        <div style={{ marginBottom: '10px' }}>Please choose a date.</div>
+        <DatePicker disabledDate={disabledDate}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
